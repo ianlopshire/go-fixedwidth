ee12c6d2909ea9fb9304c05eb6989bf0ea130aa9
Ian Lopshire <ilopshire@redventures.com>
2017-11-15 17:19:59 -0500

    Added `Unmarshal` method for parsing fixed width formatted data into structs.

---
+++ decode.go
@@ -0,0 +1,194 @@
+package fixedwidth
+
+import (
+	"bufio"
+	"bytes"
+	"encoding"
+	"io"
+	"reflect"
+	"strconv"
+	"unicode"
+)
+
+// Unmarshal parses the fixed width encoded data and stores the
+// result in the value pointed to by v. If v is nil or not a
+// pointer, Unmarshal returns an InvalidUnmarshalError.
+func Unmarshal(data []byte, v interface{}) error {
+	d := Decoder{
+		data: bufio.NewReader(bytes.NewReader(data)),
+	}
+	return d.Decode(v)
+}
+
+// A Decoder reads and decodes fixed width data from an input stream.
+type Decoder struct {
+	data *bufio.Reader
+	done bool
+}
+
+// NewDecoder returns a new decoder that reads from r.
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{
+		data: bufio.NewReader(r),
+	}
+}
+
+// An InvalidUnmarshalError describes an invalid argument passed to Unmarshal.
+// (The argument to Unmarshal must be a non-nil pointer.)
+type InvalidUnmarshalError struct {
+	Type reflect.Type
+}
+
+func (e *InvalidUnmarshalError) Error() string {
+	if e.Type == nil {
+		return "fixedwidth: Unmarshal(nil)"
+	}
+
+	if e.Type.Kind() != reflect.Ptr {
+		return "fixedwidth: Unmarshal(non-pointer " + e.Type.String() + ")"
+	}
+	return "fixedwidth: Unmarshal(nil " + e.Type.String() + ")"
+}
+
+// An UnmarshalTypeError describes a  value that was
+// not appropriate for a value of a specific Go type.
+type UnmarshalTypeError struct {
+	Value  string       // the raw value
+	Type   reflect.Type // type of Go value it could not be assigned to
+	Struct string       // name of the struct type containing the field
+	Field  string       // name of the field holding the Go value
+}
+
+func (e *UnmarshalTypeError) Error() string {
+	if e.Struct != "" || e.Field != "" {
+		return "fixedwidth: cannot unmarshal " + e.Value + " into Go struct field " + e.Struct + "." + e.Field + " of type " + e.Type.String()
+	}
+	return "fixedwidth: cannot unmarshal " + e.Value + " into Go value of type " + e.Type.String()
+}
+
+// Decode reads from its input and stores the decoded data the value
+// pointed to by v.
+//
+// In the case that v points to a struct value, Decode will read a
+// single line from the input.
+//
+// In the case that v points to a slice value, Decode will read until
+// the end of its input.
+func (d *Decoder) Decode(v interface{}) (err error) {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return &InvalidUnmarshalError{reflect.TypeOf(v)}
+	}
+
+	switch reflect.Indirect(reflect.ValueOf(v)).Kind() {
+	case reflect.Slice:
+		return d.array(reflect.ValueOf(v).Elem())
+	case reflect.Struct:
+		return d.object(reflect.ValueOf(v).Elem())
+	default:
+		return &InvalidUnmarshalError{reflect.TypeOf(v)}
+	}
+}
+
+func (d *Decoder) array(v reflect.Value) (err error) {
+	ct := v.Type().Elem()
+	for {
+		nv := reflect.New(ct).Elem()
+		err := d.object(nv)
+		if err != nil {
+			return err
+		}
+		if d.done {
+			break
+		}
+		v.Set(reflect.Append(v, nv))
+	}
+	return nil
+}
+
+func (d *Decoder) object(v reflect.Value) (err error) {
+	// TODO: properly handle prefixed lines
+	line, _, err := d.data.ReadLine()
+	if err == io.EOF {
+		d.done = true
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		fv := v.Field(i)
+		if !fv.IsValid() {
+			continue
+		}
+
+		sf := t.Field(i)
+		startPos, endPos, ok := parseTag(sf.Tag.Get("fixed"))
+		if !ok {
+			continue
+		}
+		rawValue := getRawValue(line, startPos, endPos)
+
+		if tu, ok := fv.Addr().Interface().(encoding.TextUnmarshaler); ok {
+			err := tu.UnmarshalText(rawValue)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		valid := false
+		switch sf.Type.Kind() {
+		case reflect.String:
+			valid = setString(fv, rawValue)
+		case reflect.Int:
+			valid = setInt(fv, rawValue)
+		case reflect.Float64:
+			valid = setFloat(fv, rawValue)
+		}
+		if !valid {
+			return &UnmarshalTypeError{string(rawValue), sf.Type, t.Name(), sf.Name}
+		}
+	}
+	return nil
+}
+
+func getRawValue(line []byte, startPos, endPos int) []byte {
+	if len(line) == 0 || startPos >= len(line) {
+		return []byte{}
+	}
+	if endPos > len(line) {
+		endPos = len(line)
+	}
+	return bytes.TrimLeftFunc(line[startPos-1:endPos], unicode.IsSpace)
+}
+
+func setString(fv reflect.Value, rawValue []byte) bool {
+	fv.SetString(string(rawValue))
+	return true
+}
+
+func setInt(fv reflect.Value, rawValue []byte) bool {
+	if len(rawValue) < 1 {
+		return true
+	}
+	i, err := strconv.Atoi(string(rawValue))
+	if err != nil {
+		return false
+	}
+	fv.SetInt(int64(i))
+	return true
+}
+
+func setFloat(fv reflect.Value, rawValue []byte) bool {
+	if len(rawValue) < 1 {
+		return true
+	}
+	f, err := strconv.ParseFloat(string(rawValue), 64)
+	if err != nil {
+		return false
+	}
+	fv.SetFloat(f)
+	return true
+}
---
+++ decode_test.go
@@ -0,0 +1,84 @@
+package fixedwidth_test
+
+import (
+	"github.com/ianlopshire/go-fixedwidth"
+	"reflect"
+	"testing"
+)
+
+// str is a string that implements the encoding.TextUnmarshaler interface.
+// This is useful for testing.
+type str string
+
+// UnmarshalText implements encoding.TextUnmarshaler.
+func (s *str) UnmarshalText(text []byte) error {
+	*s = str(text)
+	return nil
+}
+
+func TestUnmarshal(t *testing.T) {
+	// allTypes contains a field with all current supported types.
+	type allTypes struct {
+		String          string  `fixed:"1,5"`
+		Int             int     `fixed:"6,10"`
+		Float           float64 `fixed:"11,15"`
+		TextUnmarshaler str     `fixed:"16,20"` // test encoding.TextUnmarshaler functionality
+	}
+	for _, tt := range []struct {
+		name      string
+		rawValue  []byte
+		target    interface{}
+		expected  interface{}
+		shouldErr bool
+	}{
+		{
+			name:     "Basic Slice Case",
+			rawValue: []byte("  foo  123  1.2  bar" + "\n" + "  bar  321  2.1  foo"),
+			target:   &[]allTypes{},
+			expected: &[]allTypes{
+				{"foo", 123, 1.2, "bar"},
+				{"bar", 321, 2.1, "foo"},
+			},
+			shouldErr: false,
+		},
+		{
+			name:      "Basic Struct Case",
+			rawValue:  []byte("  foo  123  1.2  bar"),
+			target:    &allTypes{},
+			expected:  &allTypes{"foo", 123, 1.2, "bar"},
+			shouldErr: false,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			err := fixedwidth.Unmarshal(tt.rawValue, tt.target)
+			if tt.shouldErr != (err != nil) {
+				t.Errorf("Unmarshal() err want %v, have %v (%v)", tt.shouldErr, err != nil, err)
+			}
+			if !reflect.DeepEqual(tt.target, tt.expected) {
+				t.Errorf("Unmarshal() want %+v, have %+v", tt.target, tt.expected)
+			}
+
+		})
+	}
+
+	t.Run("Invalid Unmarshal Errors", func(t *testing.T) {
+		for _, tt := range []struct {
+			name      string
+			v         interface{}
+			shouldErr bool
+		}{
+			{"Invalid Unmarshal Nil", nil, true},
+			{"Invalid Unmarshal Not Pointer 1", struct{}{}, true},
+			{"Invalid Unmarshal Not Pointer 2", []struct{}{}, true},
+			{"Valid Unmarshal slice", &[]struct{}{}, false},
+			{"Valid Unmarshal struct", &struct{}{}, false},
+		} {
+			t.Run(tt.name, func(t *testing.T) {
+				err := fixedwidth.Unmarshal([]byte{}, tt.v)
+				if tt.shouldErr != (err != nil) {
+					t.Errorf("Unmarshal() err want %v, have %v (%v)", tt.shouldErr, err != nil, err)
+				}
+			})
+		}
+	})
+}
